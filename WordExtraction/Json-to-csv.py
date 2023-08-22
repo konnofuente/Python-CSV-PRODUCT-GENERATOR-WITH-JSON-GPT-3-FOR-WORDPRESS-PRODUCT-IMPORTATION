017ad5b4ca5fcb3358b5c9f951afeb03f65e6082
konnofuente <konomelifuente@gmail.com>
2023-08-22 09:06:48 +0100

    update path to save csv file

--- WordExtraction/Json-to-csv.py
+++ WordExtraction/Json-to-csv.py
@@ -1,214 +1,16 @@
-# import csv
-# import json
-
-# # Read the JSON file from your local machine
-# with open('H:\My Drive\Client\La rosee\Web developement\code\WordExtraction\ProductJSON.json') as file:
-#     data = json.load(file)
-
-# # Get the array of products from the JSON data
-# products = data
-
-# # Check if the products list is empty
-# if len(products) == 0:
-#     print("No products found in the JSON file.")
-# else:
-#     # Get all unique keys from all products
-#     keys = set()
-#     for product in products:
-#         keys.update(product.keys())
-
-#     # Create a CSV file and write the data
-#     with open('products.csv', 'w', newline='') as file:
-#         writer = csv.DictWriter(file, fieldnames=keys)
-#         writer.writeheader()
-
-#         for product in products:
-#             # Fill missing keys with None
-#             for key in keys:
-#                 product.setdefault(key, None)
-
-#             writer.writerow(product)
-
-#     print("CSV file created successfully!")
-
-
-# import csv
-# import json
-
-# # Read the JSON file from your local machine
-# with open('H:\My Drive\Client\La rosee\Web developement\code\WordExtraction\ProductJSON.json') as file:
-#     data = json.load(file)
-
-# # Get the array of products from the JSON data
-# products = data
-
-# # Check if the products list is empty
-# if len(products) == 0:
-#     print("No products found in the JSON file.")
-# else:
-#     # Get all unique keys from all products
-#     keys = set()
-#     for product in products:
-#         keys.update(product.keys())
-
-#     # Create a CSV file and write the data with 'latin-1' encoding
-#     with open('products4.csv', 'w', newline='', encoding='latin-1') as file:
-#         writer = csv.DictWriter(file, fieldnames=keys)
-#         writer.writeheader()
-
-#         for product in products:
-#             # Fill missing keys with None
-#             for key in keys:
-#                 product.setdefault(key, None)
-
-#             # Decode the product values using 'latin-1' encoding
-#             decoded_product = {key: value.decode('latin-1') if isinstance(value, bytes) else value for key, value in product.items()}
-#             writer.writerow(decoded_product)
-
-#     print("CSV file created successfully!")
-
-
-
-# import csv
-# import json
-
-# # Read the JSON file from your local machine
-# with open('H:\My Drive\Client\La rosee\Web developement\code\WordExtraction\ProductJSON.json') as file:
-#     data = json.load(file)
-
-# # Get the array of products from the JSON data
-# products = data
-
-# # Check if the products list is empty
-# if len(products) == 0:
-#     print("No products found in the JSON file.")
-# else:
-#     # Get all unique keys from all products
-#     keys = set()
-#     for product in products:
-#         keys.update(product.keys())
-
-#     # Create a CSV file and write the data with 'utf-8' encoding
-#     with open('products52.csv', 'w', newline='', encoding='utf-8') as file:
-#         writer = csv.DictWriter(file, fieldnames=keys)
-#         writer.writeheader()
-
-#         for product in products:
-#             # Fill missing keys with None
-#             for key in keys:
-#                 product.setdefault(key, None)
-
-#             # Encode the product values using unicode escape sequences
-#             encoded_product = {key: value.encode('unicode_escape').decode() if isinstance(value, str) else value for key, value in product.items()}
-#             writer.writerow(encoded_product)
-
-#     print("CSV file created successfully!")
-
-
-# import csv
-# import json
-
-# # Read the JSON file from your local machine
-# with open('H:\My Drive\Client\La rosee\Web developement\code\WordExtraction\ProductJSON2.json') as file:
-#     data = json.load(file)
-
-# # Get the array of products from the JSON data
-# products = data
-
-# # Check if the products list is empty
-# if len(products) == 0:
-#     print("No products found in the JSON file.")
-# else:
-#     # Get all unique keys from all products
-#     keys = set()
-#     for product in products:
-#         keys.update(product.keys())
-
-#     # Add 'categories' column to keys
-#     keys.add('categories')
-#     keys.add('short_description')
-
-#     # Create a CSV file and write the data with 'utf-8' encoding
-#     with open('NewProduct03.csv', 'w', newline='', encoding='utf-8') as file:
-#         writer = csv.DictWriter(file, fieldnames=keys)
-#         writer.writeheader()
-
-#         for product in products:
-#             # Fill missing keys with None
-#             for key in keys:
-#                 product.setdefault(key, None)
-
-#             # Modify 'categories' value based on 'material' or 'type' key
-#             if 'type' in product:
-#                 product['categories'] = product['material']
-#             elif 'material' in product:
-#                 product['categories'] = product['type']
-
-#             # Modify 'unite package' value
-#             if 'units_per_package' in product:
-#                 value = product['units_per_package']
-#                 product['short_description'] = f"pack de {value}"
-
-#             # Modify 'price' value
-#             if 'price' in product:
-#                 price = product['price']
-#                 if 'XAF' not in price:
-#                     product['price'] = f"XAF {price}"
-
-#             # Encode the product values using unicode escape sequences
-#             encoded_product = {key: value.encode('unicode_escape').decode() if isinstance(value, str) else value for key, value in product.items()}
-#             writer.writerow(encoded_product)
-
-#     print("CSV file created successfully!")
-
-
-# import csv
-# import json
-
-# # Read the JSON file from your local machine
-# with open('H:\My Drive\Client\La rosee\Web developement\code\WordExtraction\ProductJSON3.json') as file:
-#     data = json.load(file)
-
-# # Get the array of products from the JSON data
-# products = data
-
-# # Check if the products list is empty
-# if len(products) == 0:
-#     print("No products found in the JSON file.")
-# else:
-#     # Get all unique keys from all products
-#     keys = set()
-#     for product in products:
-#         keys.update(product.keys())
-
-#     with open('NewProduct03.csv', 'w', newline='', encoding='utf-8') as file:
-#         writer = csv.DictWriter(file, fieldnames=keys)
-#         writer.writeheader()
-
-#         for product in products:
-#             # Fill missing keys with None
-#             for key in keys:
-#                 product.setdefault(key, None)
-
-#             # Remove quotation marks from 'categories' value
-#             if 'categories' in product and isinstance(product['categories'], list):
-#                 product['categories'] = ', '.join(product['categories'])
-
-#             encoded_product = {key: value.encode('unicode_escape').decode() if isinstance(value, str) else value for key, value in product.items()}
-#             writer.writerow(encoded_product)
-
-#     print("CSV file created successfully!")
-
-
 import csv
 import json
+import os
 
 # Ask for user input
 category = input("Please enter a category: ")
 csv_file_name = input("Please enter the name of the CSV file (without extension): ")
 
+# Define the directory to save the CSV files
+csv_dir = 'CSV'
+
 # Read the JSON file from your local machine
-with open('H:\My Drive\Client\La rosee\Web developement\code\Python-CSV-PRODUCT-GENERATOR-WITH-JSON-GPT-3-FOR-WORDPRESS-PRODUCT-IMPORTATION\WordExtraction\Gobelets.json') as file:
+with open('H:\My Drive\Client\La rosee\Web developement\code\Python-CSV-PRODUCT-GENERATOR-WITH-JSON-GPT-3-FOR-WORDPRESS-PRODUCT-IMPORTATION\WordExtraction\JSON\PiqueBrochette.json') as file:
     data = json.load(file)
 
 # Get the array of products from the JSON data
@@ -223,7 +25,10 @@ else:
     for product in products:
         keys.update(product.keys())
 
-    with open(f'{csv_file_name}.csv', 'w', newline='', encoding='utf-8') as file:
+    # Make sure that the directory exists, if not create it
+    os.makedirs(csv_dir, exist_ok=True)
+
+    with open(os.path.join(csv_dir, f'{csv_file_name}.csv'), 'w', newline='', encoding='utf-8') as file:
         writer = csv.DictWriter(file, fieldnames=keys)
         writer.writeheader()
 
@@ -240,4 +45,5 @@ else:
             encoded_product = {key: value.encode('unicode_escape').decode() if isinstance(value, str) else value for key, value in product.items()}
             writer.writerow(encoded_product)
 
-    print(f"{csv_file_name}.csv file created successfully!")
+    print(f"{csv_file_name}.csv file created successfully in {csv_dir} directory!")
+
